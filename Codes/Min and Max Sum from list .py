791b4e7254f98d0e7149b03bf2bd3b645145ec22
pinakipmishra <128572937+pinakipmishra@users.noreply.github.com>
2023-03-25 13:18:57 +0530

    Min and Max sum

---
+++ Codes/Min and Max Sum from list .py
@@ -0,0 +1,7 @@
+num_List = [10,23,4,5,89,123,75]
+
+max_sum = sum(num_List) - min(num_List)
+min_sum = sum(num_List) - max(num_List)
+
+print("Max sum is {0}".format(max_sum))
+print("Min sun is {0}".format(min_sum))
