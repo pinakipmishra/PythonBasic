0d96c0f254ccb7d0b3782da1f03ad629f4999f75
Pinaki Mishra <31736811+pmpinaki@users.noreply.github.com>
2023-04-01 12:00:15 +0530

    Update FirstProgram_HelloWorld.py
    Changing the code to be more intuitive

--- Codes/FirstProgram_HelloWorld.py
+++ Codes/FirstProgram_HelloWorld.py
@@ -1,3 +1,4 @@
 # This is a first python program to print Hello World.
 # Try changing from "Hello World" to "Greeting <your name>"
-print("Hello World")
+name = input("enter your name: ")
+print("Hello {0}".format(name))
